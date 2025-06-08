a0a0f1a575136529cb3916e979ef4a1852c10545
Vansh Mishra <mishravansh831@gmail.com>
2025-06-08 17:09:12 +0530

    nav bar and about section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
- /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./src/**/*.{html,js}"],
+   content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
    theme: {
-     extend: {},
+     extend: {
+      keyframes: {
+        blob: {
+          "0%": { transform: "scale(1)" },
+          "33%": { transform: "scale(1.2)" },
+          "66%": { transform: "scale(0.8)" },
+          "100%": { transform: "scale(1)" },
+        },
+      },
+      animation: {
+        blob: "blob 10s infinite",
+      }
+     },
    },
    plugins: [],
  }
